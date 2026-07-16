e572c9d25c2a9adff32de9116df4501fc04d34da
Mohit Gurjar <mg4805556@gmail.com>
2026-07-16 20:33:21 +0530

    Add files via upload

---
+++ Day-9/bootstrap.bundle.min.js/dashboard.php/db_connect.php/footer.php/header.php/login.php/registration.php/header.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+
+<body>
+    <header class="bg-light border-bottom">
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center py-3">
+
+               <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJmtk51f0vK0dP1rdWUXfWEgxsf_OrxYiDoXehAxk3FQ&s=10" alt="" width="80">
+
+                <nav>
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <a href="index.php" class="nav-link text-dark">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="about.php" class="nav-link text-dark">About Us</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="contact.php" class="nav-link text-dark">Contact Us</a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <button type="button" class="btn btn-primary">
+                    Login
+                </button>
+
+            </div>
+        </div>
+    </header>
